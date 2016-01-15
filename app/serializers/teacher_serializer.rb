4902c462e829012eb728b62b3123d6e5ed949689
Jared Kobos <jaredkobos@gmail.com>
2016-01-15 11:14:41 +0800

    Update teacher serializer to include list of teachers

--- app/serializers/teacher_serializer.rb
+++ app/serializers/teacher_serializer.rb
@@ -1,8 +1,13 @@
 class TeacherSerializer < ActiveModel::Serializer
   attributes :id, :name, :admin
 
+  class TeacherListSerializer < ActiveModel::Serializer
+    attributes :name
+  end
+
   class SubSchoolSerializer < ActiveModel::Serializer
     has_many :classrooms
+    has_many :teachers, serializer: TeacherListSerializer
     attributes :name, :ipads
   end
 
